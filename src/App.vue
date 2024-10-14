984c48c20fa32ca973cc1a93445cd83161f0709d
Og <breezenest0912@gmail.com>
2024-10-14 14:03:09 +0800

    HTML/CSS slicing

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,81 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <main class="w-full p-5 pb-10 bg-white shadow-xl rounded-xl lg:rounded-3xl lg:p-10">
+    <h1 class="text-2xl font-bold text-center mb-10 text-primary lg:text-4xl">To Do List</h1>
+    <div class="flex gap-2 mb-10 lg:gap-5 lg:mb-20">
+      <div class="flex-1"><input type="text" aria-label="新增任務" class="w-full h-8 text-lg border border-gray-300 rounded-md px-2 lg:h-12 lg:px-5 lg:rounded-lg"></div>
+      <div class="w-14 lg:w-24"><button class="w-full bg-primary text-white text-base h-8 rounded-md lg:rounded-lg lg:text-lg lg:h-12">新增</button></div>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
+    <div class="mb-5 flex justify-end gap-4 lg:mb-10">
+      <label class="text-sm lg:text-base"><input type="checkbox" aria-label="全選"> 全選</label>
+    </div>
+
+    <div class="mb-10 border-b-2 border-primary lg:mb-20">
+      <ul class="flex">
+        <li class="flex-1"><button class="tab">全部</button></li>
+        <li class="flex-1"><button class="tab">已完成</button></li>
+        <li class="flex-1"><button class="tab">未完成</button></li>
+      </ul>
+    </div>
+
+    <div>
+      <div class="mb-10">
+        <h2 class="w-fit bg-gray-100 text-sm py-1 px-4 rounded-full text-gray-400 lg:text-base">未完成</h2>
+        <ul>
+          <template>
+            <li class="border-b border-gray-300 px-0 py-5 flex lg:p-5">
+              <div class="flex flex-1 items-center">
+                <label class="flex gap-3 lg:gap-5">
+                  <input type="checkbox">
+                  <span class="text-lg">倒垃圾</span>
+                </label>
+              </div>
+              <div class="w-6 lg:w-8">
+                <div class="w-full aspect-square rounded-full close-btns bg-red-600" aria-label="刪除任務"></div>
+              </div>
+            </li>
+          </template>
+        </ul>
+      </div>
+  
+      <div  class="mb-10">
+        <h2 class="w-fit bg-primary text-sm py-1 px-4 rounded-full text-white lg:text-base">已完成</h2>
+        <ul>
+          <template>
+            <li class="border-b border-gray-300 px-0 py-5 flex lg:p-5">
+              <div class="flex flex-1 items-center">
+                <label class="flex gap-3 lg:gap-5">
+                  <input type="checkbox">
+                  <span class="text-lg">擦窗戶</span>
+                </label>
+              </div>
+              <div class="w-6 lg:w-8">
+                <div class="w-full aspect-square rounded-full close-btns bg-red-600" aria-label="刪除任務"></div>
+              </div>
+            </li>
+          </template>
+        </ul>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.close-btns {
+  position: relative;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.close-btns::before {
+  content: '';
+  @apply absolute inset-0 m-auto h-1 w-3/6 bg-white rotate-45;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.close-btns::after {
+  content: '';
+  @apply absolute inset-0 m-auto h-1 w-3/6 bg-white -rotate-45;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/output.css'
 
 createApp(App).mount('#app')
